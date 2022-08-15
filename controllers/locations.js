e645effbb6f99dc402f5548c73c0c24f43ef738d
v-jacx <jvelasquez6798@gmail.com>
2022-08-15 15:01:06 -0700

    created hostLocation Route

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,5 +1,5 @@
 const {User, Location}=require('../models')
-
+const {cloudinary} = require('../utils/cloudinary.js')
 
 const getAllLocations = async (req,res)=>{
     try{
@@ -10,9 +10,43 @@ const getAllLocations = async (req,res)=>{
     }
 }
 
+const getLocationById = async (req, res)=>{
+    try {
+        const id = req.params.id
+        const location = await Location.findByPk(id, {include: [{model: User, as: 'host'}]})
+        res.send(location)
+    } catch (error) {
+        throw error
+    }
+}
+
+const hostLocation = async (req, res)=>{
+    try {
+        let imgUrls = [];
+        const imgStr = req.body.data
+        imgStr.map(async(str)=>{
+            const uploadedRes = await cloudinary.uploader.upload(str, {
+                upload_preset: 'ThisSite'
+            })
+            const url = uploadedRes.url
+            imgUrls.push(url)
+        })
+
+        const location = await Location.create({
+            images: imgUrls,
+            address: req.body.address,
+            description: req.body.description,
+            price: req.body.price,
+            availability: available
+        })
+    } catch (error) {
+        throw error
+    }
+}
+
 
 
 
 module.exports = {
-    getAllLocations
+    getAllLocations, getLocationById, hostLocation
 }
--- routes/LocationRouter.js
+++ routes/LocationRouter.js
@@ -2,4 +2,6 @@ const Router = require("express").Router()
 const controller = require("../controllers/locations")
 
 Router.get("/", controller.getAllLocations)
+Router.get("/:id", controller.getLocationById)
+Router.post('/host',controller.hostLocation)
 module.exports = Router
