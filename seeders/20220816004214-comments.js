84877e3a48afd62d4d7ca241f101760eac24b553
v-jacx <jvelasquez6798@gmail.com>
2022-08-15 21:04:33 -0700

    expanded getLocations to get comments

--- seeders/20220816004214-comments.js
+++ seeders/20220816004214-comments.js
@@ -1,24 +1,31 @@
-'use strict';
 
+'use strict'
+const { User, Location, sequelize } = require('../models')
+const { Op } = require('sequelize')
+const falso = require('@ngneat/falso')
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    const comments = await Promise.all(
+      [...Array(100)].map(async () => {
+        let user = await User.findOne({ order: sequelize.random(), raw: true })
+        let location = await Location.findOne({
+          order: sequelize.random(),
+          where: { user_Id: { [Op.not]: user.id } },
+          raw: true
+        })
+        return {
+          rating: falso.randNumber({min: 0, max: 5}),
+          content: falso.randParagraph(),
+          likes: falso.randNumber({ min: 0, max: 40000 }),
+          user_Id: user.id,
+          location_Id: location.id
+        }
+      })
+    )
+    return queryInterface.bulkInsert('comments', comments)
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('comments')
   }
-};
+}
