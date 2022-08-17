5f26106e6d6c943a58be52e48ddded01208528b8
v-jacx <jvelasquez6798@gmail.com>
2022-08-17 14:03:15 -0700

    password hash added to create user route

--- seeders/20220816004256-more-users.js
+++ seeders/20220816004256-more-users.js
@@ -1,13 +1,30 @@
 'use strict';
 const users = require('../data/moreUsers.json')
-
+const middleware = require('../middleware')
 module.exports = {
   async up (queryInterface, Sequelize) {
- return queryInterface.bulkInsert('users',users) 
- 
+    const newUsers = await Promise.all(
+      users.map(async(user)=>{
+        const {userName, firstName, lastName, email, phoneNumber, password} = user
+
+        const passwordDigest = await middleware.hashPassword(password)
+
+        return( 
+            {
+            userName: userName,
+            firstName: firstName,
+            lastName: lastName,
+            email:email,
+            phoneNumber:phoneNumber,
+            password:passwordDigest
+        })
+
+      })
+    )
+    return queryInterface.bulkInsert('users', newUsers)
   },
 
   async down (queryInterface, Sequelize) {
-  return queryInterface.bulkDelete('users')
+    return queryInterface.bulkDelete('users')
   }
 };
