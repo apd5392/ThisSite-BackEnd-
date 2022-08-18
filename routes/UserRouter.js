9687de79ad5571f80f82b253841a7489f3e7cef0
Carolyn <yintingchien@gmail.com>
2022-08-18 12:46:32 -0400

    fixed github merge

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,9 +1,16 @@
 const Router = require('express').Router()
 const controller = require('../controllers/users')
-
+const middleware = require('../middleware')
 Router.post('/signup', controller.createUser)
 Router.post('/login', controller.login)
 Router.put('/:id', controller.updateUser)
 Router.delete('/:id', controller.deleteUser)
+Router.get(
+  '/session',
+
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CheckSession
+)
 
 module.exports = Router
