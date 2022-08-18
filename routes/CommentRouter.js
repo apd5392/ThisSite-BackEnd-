6b5b6faf3d402fc504714503b923262b13d17ffd
v-jacx <jvelasquez6798@gmail.com>
2022-08-18 13:12:05 -0700

    likeButton route created

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -1,6 +1,7 @@
 const Router = require('express').Router()
 const controller = require('../controllers/comments.js')
 
+Router.get('/:comment_id',controller.likeButton)
 Router.post('/',controller.createComment)
 Router.put('/:comment_id', controller.updateComment)
 Router.delete('/:comment_id', controller.deleteComment)
